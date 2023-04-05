b8a73863a2b8aa56b04b62d772e71ac14dd01cc5
Rolph1 <rudzgtz@gmail.com>
2023-04-05 13:55:40 +0200

    fixed cprDecoder

--- src/ch/epfl/javions/adsb/CprDecoder.java
+++ src/ch/epfl/javions/adsb/CprDecoder.java
@@ -13,18 +13,16 @@ public class CprDecoder {
     final static double deltaPhi1 = 1 / (double) Zphi1;
 
     /**
-     * @param x0         even longitude
-     * @param y0         even latitude
-     * @param x1         uneven longitude
-     * @param y1         uneven latitude
+     * @param x0         normalized even longitude (divided by 2^17) in turn
+     * @param y0         normalized even latitude (divided by 2^17) in turn
+     * @param x1         normalized uneven longitude (divided by 2^17) in turn
+     * @param y1         normalized uneven latitude (divided by 2^17) in turn
      * @param mostRecent 0 if the most recent of position of the plane is even, 1 if it is uneven
-     * @return corresponding latitude and longitude
-     * @author Rudolf Yazbeck
+     * @return corresponding decoded latitude and longitude
+     * @author Rudolf Yazbeck (SCIPER: 360700)
      */
     public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent) {
         Preconditions.checkArgument(mostRecent == 0 || mostRecent == 1);
-        double lambda0;
-        double lambda1;
 
         int zphi = (int) Math.rint(y0 * Zphi1 - y1 * Zphi0);
         int zphi0, zphi1;
@@ -46,11 +44,14 @@ public class CprDecoder {
             phi1 = phi1 - 1;
         }
 
+        //two different constants are calculated here to calculate ZlambdaTest, the usage of which is explained below
         double A = Math.acos(1 - (1 - Math.cos(2 * Math.PI * deltaPhi0)) / Math.pow(Math.cos(Units.convert(phi0, Units.Angle.TURN, Units.Angle.RADIAN)), 2));
         double B = Math.acos(1 - (1 - Math.cos(2 * Math.PI * deltaPhi0)) / Math.pow(Math.cos(Units.convert(phi1, Units.Angle.TURN, Units.Angle.RADIAN)), 2));
 
         int Zlambda0;
-        int ZlambdaTest;
+        int ZlambdaTest; //variable to test wether A and B yield the same Zlambda0
+
+        //in case acos is given as argument a value that is not within [-1;1] then by definition Zlambda0 is equal to 1
         if (((Double) A).isNaN()) {
             Zlambda0 = 1;
             ZlambdaTest = 1;
@@ -59,58 +60,70 @@ public class CprDecoder {
             ZlambdaTest = (int) Math.floor((2 * Math.PI) / B);
         }
 
+        //in this case, the aircraft has changed its latitude band and its position cannot be determined, so we return null
         if (Zlambda0 != ZlambdaTest) {
             return null;
         }
         int Zlambda1 = Zlambda0 - 1;
 
+        double lambda; //lambda that will be used in the return statement so as to not do double the calculations
+        double phi; //same thing
+
         if (Zlambda0 == 1) {
-            lambda1 = x1;
-            lambda0 = x0;
+            if(mostRecent == 1) {
+                lambda = x1;
+                phi = phi1;
+            }
+            else {
+                phi = phi0;
+                lambda = x0;
+            }
 
         } else {
             double zLambda0;
             double zLamdba1;
             int zlamdba = (int) Math.rint(x0 * Zlambda1 - x1 * Zlambda0);
 
-            if (zlamdba < 0) {
-                zLamdba1 = zlamdba + Zlambda1;
-                zLambda0 = zlamdba + Zlambda0;
-            } else {
-                zLambda0 = zlamdba;
-                zLamdba1 = zlamdba;
-            }
-            double deltaLambda0 = 1 / (double) Zlambda0;
-            double deltaLambda1 = 1 / (double) Zlambda1;
+            if(mostRecent == 0) {
+                if (zlamdba < 0) {
+                    zLambda0 = zlamdba + Zlambda0;
+                } else {
+                    zLambda0 = zlamdba;
+                }
+                double deltaLambda0 = 1 / (double) Zlambda0;
 
-            lambda0 = deltaLambda0 * (zLambda0 + x0);
-            lambda1 = deltaLambda1 * (zLamdba1 + x1);
-            if (lambda0 >= 0.5) {
-                lambda0 = lambda0 - 1;
+                lambda = deltaLambda0 * (zLambda0 + x0);
+                phi = phi0;
             }
-            if (lambda1 >= 0.5) {
-                lambda1 = lambda1 - 1;
+            else {
+                if (zlamdba < 0) {
+                    zLamdba1 = zlamdba + Zlambda1;
+                } else {
+                    zLamdba1 = zlamdba;
+                }
+                double deltaLambda1 = 1 / (double) Zlambda1;
+
+                lambda = deltaLambda1 * (zLamdba1 + x1);
+                phi = phi1;
             }
         }
 
-        if (mostRecent == 0) {
-            try {
-                return (new GeoPos((int) Math.rint(Units.convert(lambda0, Units.Angle.TURN, Units.Angle.T32)), (int) Math.rint(Units.convert(phi0, Units.Angle.TURN, Units.Angle.T32))));
-            } catch (IllegalArgumentException i) {
-                return null;
-            }
-        } else {
-            if (!GeoPos.isValidLatitudeT32((int) Math.rint(Units.convert(lambda0, Units.Angle.TURN, Units.Angle.T32)))) {
-                System.out.println("hi");
-                return null;
-            }
 
-            try {
-                return (new GeoPos((int) Math.rint(Units.convert(lambda1, Units.Angle.TURN, Units.Angle.T32)), (int) Math.rint(Units.convert(phi1, Units.Angle.TURN, Units.Angle.T32))));
-            } catch (IllegalArgumentException i) {
-                return null;
-                //test
-            }
+        if (lambda >= 0.5) {
+            lambda = lambda - 1;
+        }
+        if (phi >= 0.5) {
+            phi = phi - 1;
+        }
+
+        if (!GeoPos.isValidLatitudeT32((int) Math.rint(Units.convert(lambda, Units.Angle.TURN, Units.Angle.T32)))) {
+            return null;
+        }
+
+        try {
+            return (new GeoPos((int) Math.rint(Units.convert(lambda, Units.Angle.TURN, Units.Angle.T32)), (int) Math.rint(Units.convert(phi, Units.Angle.TURN, Units.Angle.T32))));
+        } catch (IllegalArgumentException i) {
+            return null;
         }
     }
 }
