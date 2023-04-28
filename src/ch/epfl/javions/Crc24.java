7ac9f84d19cb3757b01a8077cb0547beddbb8805
TheoLeFur <theo.lefur@epfl.ch>
2023-04-28 10:25:16 +0200

    fix CRC24

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -10,7 +10,7 @@ public final class Crc24 {
 
     //constant that represents the 24 weak bits of the generator used to calculate the crc24 of the ADSB messages
     public final static int GENERATOR = 0xFFF409;
-    static int[] builtTable;
+    private final int[] builtTable;
 
     /**
      * @param generator key that will be used for the cyclic redundancy check
@@ -52,8 +52,8 @@ public final class Crc24 {
 
         //for loop that goes through each element of the message
         for (byte value : table) {
-            for (int j = 0; j < 8; j++) {
-                b = Bits.extractUInt(value, 7 - j, 1);
+            for (int j = 0; j < Byte.SIZE; j++) {
+                b = Bits.extractUInt(value, Byte.SIZE - 1 - j, 1);
                 crc = ((crc << 1) | b) ^ tab[Bits.extractUInt(crc, N - 1, 1)];
             }
         }
@@ -87,12 +87,12 @@ public final class Crc24 {
         for (byte aByte : bytes) {
             //now that we're working byte by byte there is a chance the corresponding integer will be negative since
             // they are signed so if the first bit of a byte is negative we add 256 to it
-            o = Bits.extractUInt((aByte >= 0) ? aByte : aByte + 256, 0, 8);
-            crc = ((crc << 8) | o) ^ builtTable[Bits.extractUInt(crc, N - 8, 8)];
+            o = Bits.extractUInt((aByte >= 0) ? aByte : aByte + 256, 0, Byte.SIZE);
+            crc = ((crc << Byte.SIZE) | o) ^ builtTable[Bits.extractUInt(crc, N - Byte.SIZE, Byte.SIZE)];
         }
 
         for (int k = 0; k < 3; k++) {
-            crc = (crc << 8) ^ builtTable[Bits.extractUInt(crc, N - 8, 8)];
+            crc = (crc << Byte.SIZE) ^ builtTable[Bits.extractUInt(crc, N - Byte.SIZE, Byte.SIZE)];
         }
 
         crc = Bits.extractUInt(crc, 0, N);
